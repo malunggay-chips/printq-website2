79bb3956d7f5f0fab3fe0dac7afe38f12c09bda0
LadiesMan217 <jutayako666999@gmail.com>
2025-10-18 03:27:50 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = "https://gtmchmkgjtsowgwrasye.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0bWNobWtnanRzb3dnd3Jhc3llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyNzIwNjgsImV4cCI6MjA3NTg0ODA2OH0.DHjs_utI3w-edQlx70cSETjKdHYk6N6rUCG-B_wrEzo"; // from Supabase settings
+export const SUPABASE_URL = "https://xgrnppxcyidsmtkhqwin.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhncm5wcHhjeWlkc210a2hxd2luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MjYwMzMsImV4cCI6MjA3NjMwMjAzM30.wV75XsSDsMJGdtWIKWlzbNC-oNAT9SmjjjLxB8NphnQ"; // from Supabase settings
 export const API_BASE = `${SUPABASE_URL}/functions/v1`;
