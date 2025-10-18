5571dfcdbabfce82c4c42d78b1a4fc3256f85f13
LadiesMan217 <jutayako666999@gmail.com>
2025-10-18 02:40:34 -0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://gtmchmkgjtsowgwrasye.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0bWNobWtnanRzb3dnd3Jhc3llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyNzIwNjgsImV4cCI6MjA3NTg0ODA2OH0.DHjs_utI3w-edQlx70cSETjKdHYk6N6rUCG-B_wrEzo"; // from Supabase settings
+export const API_BASE = `${SUPABASE_URL}/functions/v1`;
